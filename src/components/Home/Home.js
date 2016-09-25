e9f2f9c6fa5091005b14ce868c84d84b9ce366f1
formulahendry <formulahendry@gmail.com>
2016-09-25 12:43:35 +0800

    Enable GA and AddThis after window load

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,9 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Extensions List</h1>
+        <p>
+          Not see your extension? Send a PR <a href="https://github.com/formulahendry/vsce-website/blob/master/src/assets/extensions.json">here</a>, or submit a <a href="https://github.com/formulahendry/vsce-website/issues">request</a>.
+        </p>
         {extensions.map((extension, index) =>
           <div key={extension}>
             <Link to={`/history/${extension}`}>{extension}</Link>
